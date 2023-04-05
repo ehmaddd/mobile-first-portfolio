54d5611b6a0dd39d1b475f898c24e5827cffef9d
Ahmad <ehmaddd@gmail.com>
2023-04-05 17:34:18 +0500

    Add first live project

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const projects = [
   {
-    image: './images/proj-1.jpg',
-    title: 'Multi-Post Stories Gain+Glory',
-    languages: ['Ruby', 'HTML', 'CSS'],
+    image: ['./images/proj-1-1.png', './images/proj-1-2.png', './images/proj-1-3.png', './images/proj-1-4.png'],
+    title: 'Global Summit 2015',
+    languages: ['HTML5', 'CSS3', 'ES6'],
     details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.",
-    liveVersion: 'https://ehmaddd.github.io',
-    linkSource: 'https://github.com/ehmaddd/ehmaddd.github.io',
+    liveVersion: 'https://ehmaddd.github.io/online-conference/',
+    linkSource: 'https://github.com/ehmaddd/online-conference',
   },
   {
     image: './images/proj-2.jpg',
@@ -56,7 +56,20 @@ const modalUl = document.querySelector('#myModal ul');
 
 const works = document.querySelectorAll('.work-sample');
 for (let a = 0; a < works.length; a += 1) {
-  document.querySelectorAll('.work-sample img')[a].src = projects[a].image;
+  if (a === 0) {
+    let count = 0;
+    setInterval(() => {
+      document.querySelectorAll('.work-sample img')[a].src = projects[a].image[count];
+      if (count < 3) {
+        count += 1;
+      } else {
+        count = 0;
+      }
+    }, 2000);
+  } else {
+    document.querySelectorAll('.work-sample img')[a].src = projects[a].image;
+  }
+
   document.querySelectorAll('.work-sample h4')[a].innerHTML = projects[a].title;
   worksUl = document.querySelectorAll('.work-sample ul');
 
@@ -77,7 +90,23 @@ for (let a = 0; a < btn.length; a += 1) {
   btn[a].addEventListener('click', () => {
     modal.style.display = 'block';
 
-    document.querySelector('#myModal img').src = projects[a].image;
+    //document.querySelector('#myModal img').src = projects[a].image;
+    //------------------
+    if (a === 0) {
+      let count = 0;
+      setInterval(() => {
+        document.querySelector('#myModal img').src = projects[a].image[count];
+        if (count < 3) {
+          count += 1;
+        } else {
+          count = 0;
+        }
+      }, 2000);
+    } else {
+      document.querySelector('#myModal img').src = projects[a].image;
+    }
+    //------------------
+
     document.querySelector('#myModal p').innerHTML = projects[a].details;
     document.querySelector('#myModal h4').innerHTML = projects[a].title;
 
